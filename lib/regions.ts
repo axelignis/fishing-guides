e6a8f1f5c33bcd293777cd1d29b706d9902ac7da
Axel <axel.monsalve.ponce@gmail.com>
2025-08-30 16:34:32 -0400

    feat(data): add regions dataset

---
+++ lib/regions.ts
@@ -0,0 +1,17 @@
+export const regions = [
+  {
+    id: 'biobio',
+    name: 'Biobío',
+    comunas: ['Concepción', 'Talcahuano', 'Lota']
+  },
+  {
+    id: 'valparaiso',
+    name: 'Valparaíso',
+    comunas: ['Valparaíso', 'Viña del Mar', 'Concón']
+  },
+  {
+    id: 'metropolitana',
+    name: 'Metropolitana',
+    comunas: ['Santiago', 'Providencia', 'Las Condes']
+  }
+]
